f0a95e753f8bf392ae51cc3da9246bc3cf0a0662
VerheydenTim <tim.verheyden@odisee.be>
2017-02-16 15:08:32 +0100

    Bijgewerkt programma
    Uitleg correct weergegeven

--- SoftwareArduinoMeas/Program_WIFI/Program_WIFI.ino
+++ SoftwareArduinoMeas/Program_WIFI/Program_WIFI.ino
@@ -24,105 +24,55 @@ char server[] = "www.dweet.io";             // Server waar de meetwaarden naarto
 WiFiClient client;                          // client is de verwijzing die we gebruiken naar het wifi netwerk
 
 // 
-unsigned long lastConnectionTime = 0;             // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
-// the "L" is needed to use long type numbers
-// Analoge ingang declareren
-int DigitalInput = 9;//Input relatieve vochtigheid
-int DigitalInputIR = 10;//Input personendetectie
-int DigitalInputDust = 11;//Input stofsensor
-bool DigitalValue;
-int AnalogValue;
+unsigned long lastConnectionTime = 0;             // timer tussen connecties naar server
+const unsigned long postingInterval = 10L * 1000L; // delay in ms tussen connecties
+
+//Ingangen declareren
+int DigitalInput = 9;           //Input relatieve vochtigheid
+int DigitalInputIR = 10;        //Input personendetectie
+int DigitalInputDust = 11;      //Input stofsensor
 
 // Fijn stof sensor
-unsigned long duration;
-unsigned long starttime;
-unsigned long sampletime_ms = 30000;
-unsigned long lowpulseoccupancy = 0;
-float ratio = 0;
+unsigned long duration;         //Duur puls
+unsigned long starttime;        //Starttijd puls
+unsigned long sampletime_ms = 30000;    //Sample tijd puls
+unsigned long lowpulseoccupancy = 0;    //
+float ratio = 0;                        //
 float concentration = 0;
 
 // Doorsturen gegevens
-bool bBool;
+bool bBool;                       //Dit moet vervangen worden door een case structuur
 
-            /*
-            ********************************************************
-            Relative vochtigheid en temperatuur sensor communicatie
-            ********************************************************
-            */
-            byte dataFrame [5]; // het in te lezen dataframe afkomstig van de sensor
-            //
-            void start_test () {
-              //Eerste puls laag
-              digitalWrite (DigitalInput, LOW); // Geef de bus een laag signaal om data overdracht te starten
-              delay (30); // Tijd wachten om de sensor de tijd te geven om te reageren
-              //Vervolgens puls hoog
-              digitalWrite (DigitalInput, HIGH);
-              delayMicroseconds (40); // De sensor tijd geven om te reageren
-              //Pinmode aanpassen naar input
-              pinMode (DigitalInput, INPUT);
-              //wachten op puls hoog
-              while (digitalRead (DigitalInput) == HIGH);
-              delayMicroseconds (80); // Wachten tot de bus een laag signaal geeft
-              if (digitalRead (DigitalInput) == LOW);
-              delayMicroseconds (80); // Na deze wachttijd klaar om data te verzenden
-             
-              for (int i = 0; i < 5; i ++) // De 5 bytes afvragen
-                dataFrame[i] = read_data ();
-             
-              pinMode (DigitalInput, OUTPUT);
-              digitalWrite (DigitalInput, HIGH); // De bus op true zetten, een laag signaal start opnieuw de transmissie van data
-            }
-             
-            byte read_data () {
-              byte data;
-              for (int i = 0; i < 8; i ++) {
-                if (digitalRead (DigitalInput) == LOW) {
-                  while (digitalRead (DigitalInput) == LOW); // wait for 50us
-                    delayMicroseconds (30); // determine the duration of the high level to determine the data is '0 'or '1'
-                  if (digitalRead (DigitalInput) == HIGH)
-                     data |= (1 << (7-i)); // De eerste bit moet helemaal links geplaatst worden dus 7 plaatsen doorschuiven naar links |= is een of bewerking op bit niveau
-                  while (digitalRead (DigitalInput) == HIGH); // data '1 ', wait for the next one receiver
-                 }
-              }
-            return data;
-            }
+//relatieve vochtigheid + temperatuur
+byte dataFrame [5]; // het in te lezen dataframe afkomstig van de sensor
 
 void setup() {
- //Pin configuratie
- pinMode (DigitalInput, OUTPUT);
- pinMode (DigitalInputIR, INPUT);
- //Open de seriele communicatie met een baudrate van 9600
- Serial.begin(9600);
-
-
- //Fijn stof meter
-   pinMode(11,INPUT);
-  starttime = millis();
-                                                                                           
-                             // Wifi connection
-                             // attempt to connect using WPA2 encryption:
-                              Serial.println("Attempting to connect to WPA network...");
-                              status = WiFi.begin(ssid, pass);
-                            
-                              // if you're not connected, stop here:
-                              if ( status != WL_CONNECTED) {
-                               Serial.println("Couldn't get a wifi connection");
-                               while(true);
-                             }
-                             
-                              // if you are connected, print out info about the connection:
-                              else {
-                                Serial.println("Connected to network");
-                              }
-                              // print your WiFi shield's IP address:
-                                Serial.print("IP Address: ");
-                                Serial.println(WiFi.localIP());
-                                
-                              printWifiStatus();
-                  
-                     
-              
+  
+     //Pin configuratie
+     pinMode (DigitalInput, OUTPUT); 
+     pinMode (DigitalInputIR, INPUT);
+     //Open de seriele communicatie met een baudrate van 9600
+     Serial.begin(9600);
+    
+     //Fijn stof meter
+      pinMode(11,INPUT);
+      starttime = millis();
+                                                                                               
+     // Wifi connectie
+      Serial.println("Poging tot het verkrijgen van een connectie met wifi...");
+      status = WiFi.begin(ssid, pass);
+    
+      // Controleren of het systeem geconnecteerd is
+      if ( status != WL_CONNECTED) {
+       Serial.println("Connectie met wifi is niet gelukt");
+       while(true);
+     }
+      // De connectie is gelukt
+      else {
+        Serial.println("Connectie ok");
+      }
+      // Gegevens van het netwerk printen      
+      printWifiStatus();              
 }
 
 void loop() {
@@ -180,11 +130,50 @@ Serial.println (millis());
     bBool=true;
     }
    }
-  
-                   
-
- 
 }
+
+/*
+********************************************************
+Relative vochtigheid en temperatuur sensor communicatie
+********************************************************
+*/
+void start_test () {
+  //Eerste puls laag
+  digitalWrite (DigitalInput, LOW); // Geef de bus een laag signaal om data overdracht te starten
+  delay (30); // Tijd wachten om de sensor de tijd te geven om te reageren
+  //Vervolgens puls hoog
+  digitalWrite (DigitalInput, HIGH);
+  delayMicroseconds (40); // De sensor tijd geven om te reageren
+  //Pinmode aanpassen naar input
+  pinMode (DigitalInput, INPUT);
+  //wachten op puls hoog
+  while (digitalRead (DigitalInput) == HIGH);
+  delayMicroseconds (80); // Wachten tot de bus een laag signaal geeft
+  if (digitalRead (DigitalInput) == LOW);
+  delayMicroseconds (80); // Na deze wachttijd klaar om data te verzenden
+ 
+  for (int i = 0; i < 5; i ++) // De 5 bytes afvragen
+    dataFrame[i] = read_data ();
+ 
+  pinMode (DigitalInput, OUTPUT);
+  digitalWrite (DigitalInput, HIGH); // De bus op true zetten, een laag signaal start opnieuw de transmissie van data
+}
+ 
+byte read_data () {
+  byte data;
+  for (int i = 0; i < 8; i ++) {
+    if (digitalRead (DigitalInput) == LOW) {
+      while (digitalRead (DigitalInput) == LOW); // wait for 50us
+        delayMicroseconds (30); // determine the duration of the high level to determine the data is '0 'or '1'
+      if (digitalRead (DigitalInput) == HIGH)
+         data |= (1 << (7-i)); // De eerste bit moet helemaal links geplaatst worden dus 7 plaatsen doorschuiven naar links |= is een of bewerking op bit niveau
+      while (digitalRead (DigitalInput) == HIGH); // data '1 ', wait for the next one receiver
+     }
+  }
+return data;
+}
+
+/*Print wifi status*/
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -202,36 +191,27 @@ void printWifiStatus() {
   Serial.println(" dBm");
 }
 
-// this method makes a HTTP connection to the server:
+// COnnectie naar server
 void httpRequest(byte dataframe, String post) {
-  // close any connection before send a new request.
-  // This will free the socket on the WiFi shield
+  // Sluit alle huidige connecties
   client.stop();
 
   if (client.connect(server, 80)) {
-                      Serial.println("connected");
-                   
-          
-                      // Make a HTTP request:
-                      String s = post;
-                      s.concat(dataframe);
-                      Serial.println(s);
-                      client.println(s);
-                      /*              
-                           // Make a HTTP request:
-                      s = "POST /dweet/for/ArduinoTestTim?Temp=";
-                      s.concat(dataFrame [2]);
-                      Serial.println(s);
-                      client.println(s);
-                      */
-                      client.println("Host: www.dweet.io");
-                      client.println("Connection: close");
-                      client.println();
-                      // note the time that the connection was made:
-                      lastConnectionTime = millis();
-                    }
-                    else {
-                      // if you couldn't make a connection:
-                      Serial.println("connection failed");
-                    }
+      Serial.println("connected");
+         // Connectie maken naar server met correcte waarde
+      String s = post;
+      s.concat(dataframe);
+      Serial.println(s);
+      client.println(s);
+
+      client.println("Host: www.dweet.io");
+      client.println("Connection: close");
+      client.println();
+      // Tijd bijhouden van laatste connectie
+      lastConnectionTime = millis();
+    }
+    else {
+      // Als het connecteren mislukt is
+      Serial.println("connection failed");
+    }
 }
